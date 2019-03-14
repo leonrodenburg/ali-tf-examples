b92a41d9d2093b8d28303c621b5e5d90d652ec81
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-03-14 12:52:24 +0100

    Clean up lambda code

--- 2-function-compute/src/index.js
+++ 2-function-compute/src/index.js
@@ -1,10 +1,10 @@
 exports.handler = function(request, response, context) {
   response.setStatusCode(200);
   response.setHeader("Content-Type", "application/json");
-  response.setHeader("Content-Disposition", "inline");
+
   return response.send(
     JSON.stringify({
-      status: "works 2!"
+      status: "It works!"
     })
   );
 };
